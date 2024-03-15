f6b2b1288e754a93599eff0a68b839d314c30ff8
BarGenossar <bargenossar@gmail.com>
2024-03-15 19:50:44 +0200

    Add LatticeGNN class
    Delete the previous two separate classes

--- GNN_models.py
+++ GNN_models.py
@@ -6,47 +6,31 @@ import torch_geometric
 from torch.nn import Linear
 
 
-
-class LatticeGraphSAGE(torch.nn.Module):
-    def __init__(self, input_channels, hidden_channels, num_layers=3, p_dropout=0.2, seed=1):
-        super(LatticeGraphSAGE, self).__init__()
+class LatticeGNN(torch.nn.Module):
+    def __init__(self, gnn_model, input_channels, hidden_channels, num_layers=3, p_dropout=0.2, seed=2):
+        super(LatticeGNN, self).__init__()
         seed_everything(seed)
+        self.model = self._set_model(gnn_model)
         self.num_layers = num_layers
         self.p_dropout = p_dropout
         self._set_layers(input_channels, hidden_channels)
         self.out = Linear(hidden_channels, 1)
 
-    def _set_layers(self, input_channels, hidden_channels):
-        for layer_idx in range(1, self.num_layers + 1):
-            if layer_idx == 1:
-                setattr(self, f'conv{layer_idx}', SAGEConv(input_channels, hidden_channels))
-            else:
-                setattr(self, f'conv{layer_idx}', SAGEConv(hidden_channels, hidden_channels))
-        return
-
-    def forward(self, x, edge_index):
-        for layer_idx in range(1, self.num_layers + 1):
-            conv = getattr(self, f'conv{layer_idx}')
-            x = F.leaky_relu(conv(x, edge_index))
-            x = F.dropout(x, p=self.p_dropout, training=self.training)
-        return self.out(x)
-
-
-class LatticeGAT(torch.nn.Module):
-    def __init__(self, input_channels, hidden_channels, num_layers=3, p_dropout=0.2, seed=2):
-        super(LatticeGAT, self).__init__()
-        seed_everything(seed)
-        self.num_layers = num_layers
-        self.p_dropout = p_dropout
-        self._set_layers(input_channels, hidden_channels)
-        self.out = Linear(hidden_channels, 1)
+    @staticmethod
+    def _set_model(gnn_model):
+        if gnn_model == 'SAGE':
+            return SAGEConv
+        elif gnn_model == 'GAT':
+            return GATConv
+        else:
+            raise ValueError(f"Invalid GNN model: {gnn_model}")
 
     def _set_layers(self, input_channels, hidden_channels):
         for layer_idx in range(1, self.num_layers + 1):
             if layer_idx == 1:
-                setattr(self, f'conv{layer_idx}', GATConv(input_channels, hidden_channels))
+                setattr(self, f'conv{layer_idx}', self.model(input_channels, hidden_channels))
             else:
-                setattr(self, f'conv{layer_idx}', GATConv(hidden_channels, hidden_channels))
+                setattr(self, f'conv{layer_idx}', self.model(hidden_channels, hidden_channels))
         return
 
     def forward(self, x, edge_index):
