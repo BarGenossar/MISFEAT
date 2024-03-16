e95cfaf0d57a12aef71f2464d1f25d31fafd2598
BarGenossar <bargenossar@gmail.com>
2024-03-16 15:00:23 +0200

    Add NDCG@k computation

--- graph_lattice_train_and_evaluate.py
+++ graph_lattice_train_and_evaluate.py
@@ -1,5 +1,5 @@
 import torch
-from GNN_models import LatticeGNN, LatticeGraphSAGE, LatticeGAT
+from GNN_models import LatticeGNN
 from config import LatticeGeneration, GNN, Evaluation
 import tqdm
 from utils import *
@@ -22,30 +22,47 @@ def train(lattice_graph, model, optimizer, criterion):
     return loss.item()
 
 
-def test(lattice_graph, model, at_k=20):
+def test(lattice_graph, model, at_k, comb_size, feature_num):
     lattice_graph.to(device)
     model.eval()
     with torch.no_grad():
         out = model(lattice_graph.x, lattice_graph.edge_index)
-    ndcg = compute_ndcg(lattice_graph.y, out, at_k)
-    print(5*'======')
-    print(f'NDCG: {ndcg}')
+    compute_ndcg(lattice_graph.y, out, at_k, comb_size, feature_num)
     return
 
 
-def compute_dcg(ground_truth, sorted_indices):
+def get_comb_size_indices(num_nodes, comb_size, feature_num):
+    comb_size_indices = []
+    for i in range(num_nodes):
+        binary_vec = convert_decimal_to_binary(i+1, feature_num)
+        if binary_vec.count('1') == comb_size:
+            comb_size_indices.append(i)
+    return comb_size_indices
+
+
+def get_sorted_indices(score_tensor, comb_size_indices):
+    sorted_indices = torch.argsort(score_tensor, descending=True)
+    return [idx.item() for idx in sorted_indices if idx.item() in comb_size_indices]
+
+
+def compute_dcg(ground_truth, sorted_indices, at_k):
     DCG = 0
-    for i in range(1, len(sorted_indices) + 1):
+    for i in range(1, at_k + 1):
         DCG += (ground_truth[sorted_indices[i-1]].item() / math.log2(i+1))
     return DCG
 
 
-def compute_ndcg(ground_truth, predictions, at_k):
-    sorted_gt_indices = torch.argsort(ground_truth, descending=True)[:at_k]
-    sorted_pred_indices = torch.argsort(predictions, descending=True)[:at_k]
-    IDCG = compute_dcg(ground_truth, sorted_gt_indices)
-    DCG = compute_dcg(ground_truth, sorted_pred_indices)
-    return round(DCG / IDCG, 4)
+def compute_ndcg(ground_truth, predictions, at_k, comb_size, feature_num):
+    comb_size_indices = get_comb_size_indices(len(predictions), comb_size, feature_num)
+    sorted_gt_indices = get_sorted_indices(ground_truth, comb_size_indices)
+    sorted_pred_indices = get_sorted_indices(predictions, comb_size_indices)
+    if type(at_k) is not list:
+        at_k = [at_k]
+    for k in at_k:
+        IDCG = compute_dcg(ground_truth, sorted_gt_indices, k)
+        DCG = compute_dcg(ground_truth, sorted_pred_indices, k)
+        print(f'NDCG@{k}: {round(DCG / IDCG, 4)}')
+    return
 
 
 if __name__ == "__main__":
@@ -63,6 +80,7 @@ if __name__ == "__main__":
     epochs = GNN.epochs
 
     at_k = Evaluation.at_k
+    comb_size = Evaluation.comb_size
 
     dataset_path = f"GeneratedData/Formula{formula_idx}/Config{hyperparams_idx}/dataset.pkl"
     lattice_path = f"GeneratedData/Formula{formula_idx}/Config{hyperparams_idx}/dataset_lattice.pt"
@@ -77,8 +95,8 @@ if __name__ == "__main__":
     for epoch in range(1, epochs + 1):
         loss_val = train(lattice_graph, model, optimizer, criterion)
         print(f'Epoch: {epoch}, Loss: {round(loss_val, 4)}')
-
-    test(lattice_graph, model, at_k)
+    print(5*'====================')
+    test(lattice_graph, model, at_k, comb_size, feature_num)
 
 
 
