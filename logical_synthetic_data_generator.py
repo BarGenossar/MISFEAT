febccf05ff6e3f5a4e53ba0f8600d7f0ee7454b5
ben-eliav <eliav123.spam@gmail.com>
2024-03-19 15:16:25 +0200

    added support for non-binary features, parentheses and some documentation. added hyperparameter of feature_range.

--- logical_synthetic_data_generator.py
+++ logical_synthetic_data_generator.py
@@ -3,20 +3,22 @@ import pickle
 import numpy as np
 import os
 import pandas as pd
-from itertools import combinations
-from sklearn.metrics import mutual_info_score
 
 
 class LogicalDatasetGenerator:
+    OP_DICT = {'and': np.bitwise_and, 'or': np.bitwise_or, 'xor': np.bitwise_xor, '+': np.add, '-': np.subtract,
+               '*': np.multiply, '^': np.power}
+    OPERATIONS = list(OP_DICT.keys())
+
     def __init__(self, formula_name, config_num, formula, hyperparams):
         np.random.seed(int(config_num))
         self.seed = int(config_num)
         self.name = formula_name
         self.subgroups_num = hyperparams['subgroups_num']
         self.feature_num = hyperparams['feature_num']
+        self.feature_range = hyperparams['feature_range']  # requirement - list from 0 to n with increments of 1
         self.formula = formula
         self.hyperparams = hyperparams
-        self.operation_pool = ['and', 'or', 'xor']
         self.feature_pool = [f'x_{i}' for i in range(self.feature_num)]
         self.relevant_features = self._get_relevant_features()
         self.redundant_features = self._get_redundant_features()
@@ -25,28 +27,46 @@ class LogicalDatasetGenerator:
         self.dataset = self.generate_values()
 
     def _get_relevant_features(self):
-        relevant_features = [expr for expr in self.formula.split(' ') if expr not in self.operation_pool]
+        """
+        Extracts the relevant features from the formula and removes them from the feature pool.
+        """
+        relevant_features = [expr for expr in self.formula.split(' ') if expr not in self.OPERATIONS and expr not
+                             in ['(', ')']]
         self.feature_pool = [f for f in self.feature_pool if f not in relevant_features]
         return relevant_features
 
     def _get_redundant_features(self):
+        """
+        Randomly selects a number of features to be redundant and generates a formula for each of them,
+        removing them from the feature pool.
+        """
         redundant_features = list(np.random.choice(self.feature_pool, self.hyperparams['redundant_num'], replace=False))
         self.feature_pool = [f for f in self.feature_pool if f not in redundant_features]
-        min_subset_size = self.hyperparams['redundant_min_subgroup']
-        max_subset_size = self.hyperparams['redundant_max_subgroup']
-        redundant_dict = {f: self._generate_redundant_expression(min_subset_size, max_subset_size)
-                          for f in redundant_features}
+        redundant_dict = self._generate_redundant_expression(redundant_features)
         return redundant_dict
 
-    def _generate_redundant_expression(self, min_subset_size, max_subset_size):
-        subset_size = np.random.randint(min_subset_size, max_subset_size + 1)
-        subset = np.random.choice(self.relevant_features, subset_size)
-        operations = np.random.choice(self.operation_pool, subset_size - 1)
-        interlacing_string = ' '.join([f'{subset[i]} {operations[i]}'
-                                       for i in range(subset_size - 1)]) + f' {subset[-1]}'
-        return interlacing_string
+    def _generate_redundant_expression(self, redundant_features):
+        """
+        Generates a random expression for a redundant feature, containing some combination of relevant features.
+        """
+        min_subset_size = self.hyperparams['redundant_min_subgroup']
+        max_subset_size = self.hyperparams['redundant_max_subgroup']
+        expressions = {}
+
+        for f in redundant_features:
+            subset_size = np.random.randint(min_subset_size, max_subset_size + 1)
+            subset = np.random.choice(self.relevant_features, subset_size)
+            operations = np.random.choice(self.OPERATIONS, subset_size - 1)
+            interlacing_string = ' '.join([f'{subset[i]} {operations[i]}'
+                                           for i in range(subset_size - 1)]) + f' {subset[-1]}'
+            expressions[f] = interlacing_string
+        return expressions
 
     def _get_correlated_features(self):
+        """
+        Randomly selects a number of features to be correlated with the label and generates a probability for each
+        feature to be equal to the label, removing them from the feature pool.
+        """
         correlated_features = list(np.random.choice(self.feature_pool,
                                    self.hyperparams['correlated_num'], replace=False))
         self.feature_pool = [f for f in self.feature_pool if f not in correlated_features]
@@ -56,10 +76,14 @@ class LogicalDatasetGenerator:
         return correlated_dict
 
     def generate_values(self):
+        """
+        Creates the dataframe by generating random values for the features and computing the label according to the
+        formula. Then, it adds the remaining values according to the hyperparameters.
+        """
         sample_size = self.hyperparams['sample_size']
         tmp_dataset = dict()
         for feature in self.relevant_features:
-            tmp_dataset[feature] = np.random.randint(0, 2, sample_size)
+            tmp_dataset[feature] = np.random.choice(self.feature_range, sample_size)
         tmp_dataset['y'] = self._compute_by_formula(tmp_dataset, self.formula)
         tmp_dataset = self._modify_by_features(tmp_dataset, sample_size)
         df = self._generate_dataframe(tmp_dataset)
@@ -79,27 +103,53 @@ class LogicalDatasetGenerator:
         df = self._add_random_noise(df)
         return df
 
-    def _compute_by_formula(self, data, formula):
-        expressions = formula.split(' ')
-        op_dict = {'and': np.logical_and, 'or': np.logical_or, 'xor': np.logical_xor, 'not': np.logical_not}
-        clause_results = []
-        for i, clause in enumerate(expressions):
-            if clause not in self.operation_pool:
-                clause_results.append(data[clause])
+    def infix_to_postfix(self, expression):
+        precedence = {'and': 3, 'or': 2, 'xor': 4, '+': 1, '-': 1, '*': 2, '^': 5}
+        output = []
+        operators = []
+        formula = expression.split(' ')
+        for token in formula:
+            if token not in self.OPERATIONS and token not in '()':
+                output.append(token)
+            elif token == '(':
+                operators.append(token)
+            elif token == ')':
+                while operators[-1] != '(':
+                    output.append(operators.pop())
+                operators.pop()
             else:
-                op_func = op_dict[clause]
+                while operators and operators[-1] != '(' and precedence[operators[-1]] >= precedence[token]:
+                    output.append(operators.pop())
+                operators.append(token)
+        while operators:
+            output.append(operators.pop())
+        return output
+
+    def _compute_by_formula(self, dataset, formula):
+        reverse_polish = self.infix_to_postfix(formula)
+
+        clause_results = []
+        for clause in reverse_polish:
+            print(clause)
+            if clause not in self.OPERATIONS:
+                clause_results.append(dataset[clause])
+            else:
+                op_func = self.OP_DICT[clause]
+                clause2_result = clause_results.pop()
                 clause1_result = clause_results.pop()
-                clause2_result = data[expressions[i + 1]]
-                result = op_func(clause1_result, clause2_result)
+                if clause in '+-*/^%':
+                    result = np.mod(op_func(clause1_result, clause2_result), len(self.feature_range))
+                else:
+                    result = op_func(clause1_result, clause2_result)
                 clause_results.append(result)
-                expressions.pop(i + 1)
+            print(clause_results)
         return clause_results[0].astype(int)
 
     def _get_correlated_vals(self, dataset, sample_size):
         for feature in self.correlated_features:
             dataset[feature] = np.where(np.random.rand(sample_size) <
                                         self.correlated_features[feature],
-                                        dataset['y'], 1 - dataset['y'])
+                                        dataset['y'], np.random.choice(self.feature_range))
         return dataset
 
     def _get_redundant_vals(self, dataset, sample_size):
@@ -107,12 +157,12 @@ class LogicalDatasetGenerator:
         for feature in self.redundant_features:
             computed_vals = self._compute_by_formula(dataset, self.redundant_features[feature])
             dataset[feature] = np.where(np.random.rand(sample_size) > redundant_flip_prob,
-                                        computed_vals, 1 - computed_vals)
+                                        computed_vals, np.random.choice(self.feature_range))
         return dataset
 
     def _get_noisy_vals(self, dataset, sample_size):
         for feature in self.noisy_features:
-            dataset[feature] = np.random.randint(0, 2, sample_size)
+            dataset[feature] = np.random.choice(self.feature_range, sample_size)
         return dataset
 
     def _add_random_noise(self, df):
@@ -124,7 +174,7 @@ class LogicalDatasetGenerator:
             for feature in feature_list:
                 noise_flip_prob = np.random.normal(random_noise_mean, random_noise_std)
                 tmp_df[feature] = np.where(np.random.rand(len(tmp_df)) > noise_flip_prob,
-                                           tmp_df[feature], 1 - tmp_df[feature])
+                                           tmp_df[feature], np.random.choice(self.feature_range))
             df.loc[tmp_df.index] = tmp_df
         return df
 
@@ -164,9 +214,11 @@ class LogicalDatasetGenerator:
 
 if __name__ == '__main__':
     configs = json.load(open('data_generation_config.json'))
-    for formula in configs['formulas']:
-        for config_num in configs['hyperparams']:
-            dataset = LogicalDatasetGenerator(formula, config_num, configs['formulas'][formula]['formula'],
-                                              configs['hyperparams'][config_num])
-            dataset.save()
-
+    for formula_idx in configs['formulas']:
+        for config in configs['hyperparams']:
+            data = LogicalDatasetGenerator(formula_idx, config, configs['formulas'][formula_idx]['formula'],
+                                           configs['hyperparams'][config])
+            print(data.infix_to_postfix(data.formula))
+            data.save()
+            break
+        break
