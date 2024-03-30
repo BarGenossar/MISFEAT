2bee6fac2e27f05f39c29936b706f330452933ee
ben-eliav <eliav123.spam@gmail.com>
2024-03-30 22:03:11 +0300

    sampler specific for each subpop

--- sampler.py
+++ sampler.py
@@ -4,11 +4,11 @@ from utils import convert_decimal_to_binary
 
 
 class NodeSampler:
-    def __init__(self, subgroups, config_num, rel_indices_dict, sampling_ratio, sampling_method='random'):
+    def __init__(self, subgroups, config_num, non_missing_indices, sampling_ratio, sampling_method='random'):
         np.random.seed(int(config_num))
         self.seed = int(config_num)
         self.subgroups = subgroups
-        self.rel_indices_dict = rel_indices_dict
+        self.non_missing_idx = non_missing_indices
         self.sampling_ratio = sampling_ratio
         self.sampling_method = sampling_method
         self.selected_samples = self._get_selected_samples(sampling_method)
@@ -22,9 +22,11 @@ class NodeSampler:
         return sampling_funcs[sampling_method]()
 
     def _random_sampling(self):
-        selected_samples = dict()
-        for subgroup in self.subgroups:
-            candidate_indices = self.rel_indices_dict[subgroup]
-            num_samples = int(self.sampling_ratio * len(candidate_indices))
-            selected_samples[subgroup] = np.random.choice(candidate_indices, num_samples, replace=False)
+        # selected_samples = dict()
+        num_samples = int(self.sampling_ratio * len(self.non_missing_idx))
+        selected_samples = np.random.choice(self.non_missing_idx, num_samples, replace=False)
+        # for subgroup in self.subgroups:
+        #     candidate_indices = self.rel_indices_dict[subgroup]
+        #     num_samples = int(self.sampling_ratio * len(candidate_indices))
+        #     selected_samples[subgroup] = np.random.choice(candidate_indices, num_samples, replace=False)
         return selected_samples
