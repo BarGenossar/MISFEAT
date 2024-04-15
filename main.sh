2df8010d07354fee57ae4a45a4991767d4460278
BarGenossar <bargenossar@gmail.com>
2024-04-16 02:25:05 +0300

    Add some optimizations

--- main.sh
+++ main.sh
@@ -1,26 +1,27 @@
 #!/bin/zsh
 
-formulas=(1 2 3 4 5 6);
+formulas=(0 1 2 3 4 5 6);
 configs=(1 2 3);
 hidden=(32 64 128);
 
-if [ -z "$1" ]; then
-  K=4
-else
-  K=$1
-fi
+#if [ -z "$1" ]; then
+#  K=4
+#else
+#  K=$1
+#fi
 
-echo "Beginning feature selection task with K=$K"
+# echo "Beginning feature selection task with K=$K"
 
-for i in "${formulas[@]}"; do
-  for j in "${configs[@]}"; do
+for j in "${configs[@]}"; do
+  for i in "${formulas[@]}"; do
     echo "Generating data for formula $i and config $j"
-    python3 logical_synthetic_data_generator.py --formula $i --config $j
+    python logical_synthetic_data_generator.py --formula $i --config $j
     echo "Generating lattice for formula $i and config $j"
-    python3 lattice_generator.py --formula $i --config $j
-    for k in "${hidden[@]}"; do
-      echo "Training model for formula $i, config $j and hidden_layer $k. This may take some time."
-      python3 graph_lattice_train_and_evaluate.py --formula $i --config $j --hidden_channels $k --comb_size $K
-    done
+    python lattice_graph_generator_multiprocessing.py --formula $i --config $j
+#    for k in "${hidden[@]}"; do
+#      echo "Training model for formula $i, config $j and hidden_layer $k. This may take some time."
+#      python3 graph_lattice_train_and_evaluate.py --formula $i --config $j --hidden_channels $k --comb_size $K
+#    done
+
   done
 done
